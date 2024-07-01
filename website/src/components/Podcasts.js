1637a3bd64793c1bfb16d300acd0a8760998d658
Biruk Amene <bsa16@duke.edu>
2024-07-01 14:13:14 -0400

    finished about u sfor now

--- website/src/components/Podcasts.js
+++ website/src/components/Podcasts.js
@@ -7,7 +7,7 @@ import Chandler from '../images/Chandler.png'
 import Jordan from '../images/Jordan.png'
 import Devin from '../images/Devin.png'
 import Dewayne from '../images/Dewayne.png'
-import podcastvid from '../images/podcastvid.mp4'
+import podcastvid from '../images/possible_background1.mp4'
 function Podcasts() {
   return (
     <div>
@@ -19,23 +19,30 @@ function Podcasts() {
         <div className = "podcast-overlay"> </div>
           <video src={podcastvid} className="podcast-vid" autoPlay loop muted />
         </div>
+
       <div className = "podcast-square-one">
         <img src = {Devin} className = "podcast-image-one"></img>
+        <div className = "podcast-title-one">MLB Goals, National Ranking, Recruiting Process</div>
       </div>
       <div className = "podcast-square-two">
       <img src = {Dewayne} className = "podcast-image-two"></img>
+      <div className = "podcast-title-two">NFL Draft Process, Duke Legacy & Leadership</div>
       </div>
       <div className = "podcast-square-three">
       <img src = {Aminah} className = "podcast-image-three"></img>
+      <div className = "podcast-title-three">Olympic National Team, Winning Culture, Walk Up Songs</div>
       </div>
       <div className = "podcast-square-four">
       <img src = {Ashlon} className = "podcast-image-four"></img>
+      <div className = "podcast-title-four">Team USA, Women in Sports & Recruiting Process</div>
       </div>
       <div className = "podcast-square-five">
       <img src = {Chandler} className = "podcast-image-five"></img>
+      <div className = "podcast-title-five">2023 Season, Transfer Portal & Football Chemistry</div>
       </div>
       <div className = "podcast-square-six">
       <img src = {Jordan} className = "podcast-image-six"></img>
+      <div className = "podcast-title-six">NFL Dreams, Brotherhood & New Era of Duke Football</div>
       </div>
       </div>
 
